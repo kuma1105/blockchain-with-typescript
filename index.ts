0fb2420a327ec7ead6810c8525d504197da7dee4
Fancyee <74527909+kuma1105@users.noreply.github.com>
2023-01-17 17:37:26 +0900

    readonly, Tuple, any

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@
 // boolean[]
 // number[]
 // string[]
-// tuple
+// Tuple : 특정 위치에 특정 타입이 있어야만 한다.
 // enum
 // any
 // object
@@ -12,6 +12,7 @@
 // null
 // void
 // never
+// unknown
 
 
 let a = 1; // 가능한 한 TypeScript가 추론하게 만들면 좋다.
@@ -35,11 +36,11 @@ if(playerNico.age && playerNico.age < 10) {
     console.log("1");
 }
 
-//
+// type, readonly
 
 type Age = number;
 type Player = {
-    name : string,
+    readonly name : string,
     age? : Age // ? -> number OR undefined
 }
 
@@ -59,3 +60,21 @@ const playerMaker2 = (name:string) : Player => ({name})
 
 const nico = playerMaker("nico")
 nico.age = 12
+
+// 
+
+const numbers : readonly number[] = [1, 2, 3, 4]
+// number.push(1)
+
+const names: readonly string[] = ["1","2"]
+// names.push("3")
+
+//  Tuple
+const playerB: readonly [string, number, boolean] = ["apple", 12, false]
+// playerB[0] = "2"
+
+let aaa : undefined = undefined;
+let bbb : null = null;
+
+let ccc : any = 1
+ccc = "!"
